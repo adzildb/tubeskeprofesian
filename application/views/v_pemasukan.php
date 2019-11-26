c243d0315dac7655f2c1a2f54851ec17ce958e61
adzildb <adzil.db@gmail.com>
2019-11-26 20:34:26 +0700

    Update v_pemasukan.php

--- application/views/v_pemasukan.php
+++ application/views/v_pemasukan.php
@@ -21,9 +21,18 @@
 				  <div class="col-sm-9 mb-3 mb-sm-0">
 				  <h6 class="h6 text-gray-900">Bulan</h6>
                   <select name="bulan" class="form-control">
-                    <option value="Kemeja">Kemeja</option>
-                    <option value="Baju">Baju</option>
-                    <option value="Celana">Celana</option>
+                    <option value="Januari">Januari</option>
+                    <option value="Februari">Februari</option>
+                    <option value="Maret">Maret</option>
+                    <option value="April">April</option>
+                    <option value="Mei">Mei</option>
+                    <option value="Juni">Juni</option>
+                    <option value="Juli">Juli</option>
+                    <option value="Agustus">Agustus</option>
+                    <option value="September">September</option>
+                    <option value="Oktober">Oktober</option>
+                    <option value="November">November</option>
+                    <option value="Desember">Desember</option>
                   </select>
                 </div>
                 </div>

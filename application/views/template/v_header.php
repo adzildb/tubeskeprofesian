b70338a9eb545608d3fb9b7e3fc356495ebc23d8
adzildb <adzil.db@gmail.com>
2019-11-26 19:01:47 +0700

    Update v_header.php

--- application/views/template/v_header.php
+++ application/views/template/v_header.php
@@ -67,9 +67,9 @@
           </a> 
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <a class="collapse-item" href="buttons.html">Kas Debit</a>
+            <a class="collapse-item" href="cards.html">Kas Kredit</a>
+            <a class="collapse-item" href="cards.html">Lihat Rekap Bulanan</a>
           </div>
           </div>
         </div>

01decc524e9331ad1cc816e7fe703b3a25f43d2e
fatahillahardhi <fatahillahardhi@outlook.com>
2019-11-26 20:44:16 +0700

    update

--- application/views/v_spp_debit.php
+++ application/views/v_spp_debit.php
@@ -3,10 +3,9 @@
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Debit</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            <h1 class="h3 mb-0 text-gray-800">Kas Debit</h1>
           </div>
-
+			<p class="mb-4">Halaman untuk melakukan input Kas Debit pada Kas SPP</p>
           <!-- Content Row -->
           <div class="row">
             <!-- Area Chart -->
@@ -22,17 +21,17 @@
                <form class="user">
                 <div class="form-group">
 				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 text-gray-900">Tanggal</h6> <input type="date" class="form-control" id="exampleFirstName" placeholder="Tanggal Transaksi">
+				  <h6 class="h6 font-weight-bold text-gray-900">Tanggal</h6> <input type="date" class="form-control" id="exampleFirstName" placeholder="Tanggal Transaksi">
                   </div>
                 </div>
                <div class="form-group">
 				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 text-gray-900">Keterangan</h6> <input type="text" class="form-control" id="exampleFirstName" placeholder="Keterangan">
+				  <h6 class="h6 font-weight-bold text-gray-900">Keterangan</h6> <input type="text" class="form-control" id="exampleFirstName" placeholder="Keterangan">
                   </div>
                </div>
 			   <div class="form-group">
 				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 text-gray-900">Kas Debit</h6> <input type="number" class="form-control" id="exampleFirstName" placeholder="Nominal">
+				  <h6 class="h6 font-weight-bold  text-gray-900">Kas Debit</h6> <input type="number" class="form-control" id="exampleFirstName" placeholder="Nominal">
                   </div>
                 </div>
 				<div class="form-group">

b158712b266f90039db63d2d536ad4f6d0c7e22d
adzildb <adzil.db@gmail.com>
2019-11-26 20:22:41 +0700

    Update v_pemasukan.php

--- application/views/v_pemasukan.php
+++ application/views/v_pemasukan.php
@@ -29,19 +29,35 @@
                   </div>
                 </div>
               </form>
-              <form class="user">
-                <div class="form-group">
-				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 text-gray-900">Bulan</h6> <input type="date" class="form-control" id="exampleFirstName" placeholder="Tanggal Transaksi">
-                  </div>
-                </div>
-               
-				<div class="form-group">
-				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <button class="btn btn-primary" id="exampleFirstName">Submit</button>
-                  </div>
-                </div>
-              </form>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
                 </div>
               </div>
             </div>

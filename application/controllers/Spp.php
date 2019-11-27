27e21606ce4c5ea81d10f25d7308dc383eef3a20
fatahillahardhi <fatahillahardhi@outlook.com>
2019-11-28 00:59:13 +0700

    spp

--- application/controllers/Spp.php
+++ application/controllers/Spp.php
@@ -2,7 +2,7 @@
 Class Spp extends CI_Controller{
     function __construct(){
         parent::__construct();
-
+		$this->load->model("M_spp");
     }
 
     function debit(){
@@ -17,9 +17,38 @@ Class Spp extends CI_Controller{
         $this->load->view('template/v_footer');
     }
 	
-	function rekap(){
+	public function rekap(){
+		// $bulan = $this->input->get('bulan');
+		// $data = $this->M_spp->find($bulan);
+		
+		$bulan = $this->input->get('bulan');
+		$data['bulan'] = $this->M_spp->find($bulan);
         $this->load->view('template/v_header');
-        $this->load->view('v_spp_rekap');
+        $this->load->view('v_spp_rekap', $data);
         $this->load->view('template/v_footer');
     }
+	
+	function do_add_debit() {
+		$post	= $this->input->post(NULL,TRUE);
+		$post['status'] = 'Kas Debit';
+		$tanggal = $post['tanggal'];
+		$tanggal = date('Y-m-d', strtotime($tanggal));
+		$this->M_spp->create($post);
+		
+		redirect ("Spp/debit");
+	}
+	
+	function do_add_credit() {
+		$post	= $this->input->post(NULL,TRUE);
+		$post['status'] = 'Kas Credit';
+		$tanggal = $post['tanggal'];
+		$tanggal = date('Y-m-d', strtotime($tanggal));
+		$this->M_spp->create($post);
+		
+		redirect ("Spp/credit");
+	}
+	
+	function view(){
+		$data['tanggal'] = $this->M_spp->find();
+	}
 }
--- application/views/v_spp_credit.php
+++ application/views/v_spp_credit.php
@@ -13,25 +13,25 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Input Kas Debit</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Input Kas Credit</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
 				
-               <form class="user">
+               <form class="user" action="<?= site_url("spp/do_add_credit/")?>" method="post" enctype="multipart/form-data">
                 <div class="form-group">
 				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 font-weight-bold text-gray-900">Tanggal</h6> <input type="date" class="form-control" id="exampleFirstName" placeholder="Tanggal Transaksi">
+				  <h6 class="h6 font-weight-bold text-gray-900">Tanggal</h6> <input type="date" class="form-control" id="exampleFirstName" name="tanggal" placeholder="Tanggal Transaksi">
                   </div>
                 </div>
                <div class="form-group">
 				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 font-weight-bold text-gray-900">Keterangan</h6> <input type="text" class="form-control" id="exampleFirstName" placeholder="Keterangan">
+				  <h6 class="h6 font-weight-bold text-gray-900">Keterangan</h6> <input type="text" class="form-control" id="exampleFirstName" name="keterangan" placeholder="Keterangan">
                   </div>
                </div>
 			   <div class="form-group">
 				  <div class="col-sm-9 mb-3 mb-sm-0">
-				  <h6 class="h6 font-weight-bold  text-gray-900">Kas Credit</h6> <input type="number" class="form-control" id="exampleFirstName" placeholder="Nominal">
+				  <h6 class="h6 font-weight-bold  text-gray-900">Kas Credit</h6> <input type="number" class="form-control" id="exampleFirstName" name="nominal" placeholder="Nominal">
                   </div>
                 </div>
 				<div class="form-group">
